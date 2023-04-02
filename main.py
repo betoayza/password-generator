f34941c2fe287620382c68aaf064cc6759df436f
betoayza <aaayza@gmail.com>
2023-04-02 20:05:30 -0300

    Alternate lower & capitalized

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ import secrets
 
 print("\nWellcome to Password Generator 2023!")
 
+
+def format_new_password(password):
+    new_password = ""
+    list_chars = []
+
+    # Extract chars
+    i = 0
+    while i < len(password):
+        if password[i].isalpha():
+            if len(list_chars) > 0 and list_chars[-1].islower():
+                list_chars.append(password[i].upper())
+            else:
+                list_chars.append(password[i])
+        i += 1
+
+    # Update password
+    j = 0
+    for i in range(0, len(password)):
+        if password[i].isalpha():
+            while j < len(list_chars):
+                new_char = list_chars[j]
+                new_password += new_char
+                j += 1
+                break
+        else:
+            new_password += password[i]
+    return new_password
+
+
 is_running = True
 while is_running:
     try:
@@ -31,9 +60,12 @@ while is_running:
             print("\nInvalid option!")
             continue
 
+        # alternate lowers and capitalizeds
+        new_password = format_new_password(new_password)
+
         print("\nSuccessful! New password is:", new_password)
         is_running = False
-        
+
     except:
         print("\nThat's not a number, ha!")
 
